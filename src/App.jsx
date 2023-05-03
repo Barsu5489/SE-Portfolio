fbff08edef34f29ac0b381eef8569559b7f4f1c7
barsu5489 <ebarsulai@gmail.com>
2023-05-03 19:54:24 +0300

    History counter

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+  const [count, setCount] = useState(1996)
+  const [showNewPage, setShowNewPage] = useState(false)
+
+  useEffect(() => {
+    const countInterval = setInterval(() => {
+      setCount((count) => {
+        if (count < 2023) {
+          return count + 1
+        } else {
+          clearInterval(countInterval)
+          return count
+        }
+      })
+    }, count === 1996 ||count === 2021 ||  count === 2022 || count === 2023 ? 2000 : 200)
+    return () => clearInterval(countInterval)
+  }, [count])
+
+  useEffect(() => {
+    if (count === 2023) {
+      setTimeout(() => setShowNewPage(true), 2000)
+    }
+  }, [count])
+
+  let history
+  if (count < 2000) {
+    history = 'Was Born'
+  } else if (count >= 2000 && count <= 2004) {
+    history = 'Started School'
+  } else if (count > 2004 && count <= 2007) {
+    history = 'Learnt to ride a bike'
+  } else if (count > 2007 && count <= 2014) {
+    history = 'Played basket ball'
+  } else if (count > 2014 && count <= 2018) {
+    history = 'Graduated HighSchool'
+  } else if (count > 2018 && count <= 2021) {
+    history = 'JKUAT BSC Information Technology Graduate'
+  } else if (count === 2022) {
+    history = 'Joined Moringa to Study Software Engineering'
+  } else if (count === 2023) {
+    history = 'Job Hunting - Hire Me'
+  }
+
+  return (
+    <>
+      {!showNewPage ? (
+        <div className='landing_counter_container'>
+          <div className='landing_counter'>
+            {count}
+            <hr />
+            {history}
+          </div>
+        </div>
+      ) : (
+        <>
+        
+        <div className='new-page'>
+        <Navbar />
+          <h1>New page</h1>
+          {/* Your new page content goes here */}
+        </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div> <h1>Emmanuel</h1></div>
+        <div> 
+            <ul className='nav_items'>
+                <li>About</li>
+                <li>Works</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        </div>
+  )
+}
+
+export default Navbar
